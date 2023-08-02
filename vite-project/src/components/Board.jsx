77d11c50c4b490b7552f94df50dc69aeaf6a19b9
sarthaksarangi <sarthakrohan5@gmail.com>
2023-08-03 00:58:25 +0530

    Completed: board logic

--- vite-project/src/components/Board.jsx
+++ vite-project/src/components/Board.jsx
@@ -5,19 +5,27 @@ import { useState } from "react";
 const Board = () => {
   const [state, setState] = useState(Array(9).fill(null));
   const [isXTurn, setXTurn] = useState(true);
+  const checkWinner = () => {
+    const winnerLogic = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let logic of winnerLogic) {
+      const [a, b, c] = logic;
+      if (state[a] != null && state[a] == state[b] && state[b] == state[c]) {
+        return state[a];
+      }
+    }
+    return false;
+  };
 
-  const winnerLogic = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  
-
+  const isWinner = checkWinner();
 
   console.log(state);
 
@@ -30,21 +38,40 @@ const Board = () => {
   };
   return (
     <div className="board-container">
-      <div className="board-rows">
-        <Square value={state[0]} onClick={() => handleClick(0)} />
-        <Square value={state[1]} onClick={() => handleClick(1)} />
-        <Square value={state[2]} onClick={() => handleClick(2)} />
-      </div>
-      <div className="board-rows">
-        <Square value={state[3]} onClick={() => handleClick(3)} />
-        <Square value={state[4]} onClick={() => handleClick(4)} />
-        <Square value={state[5]} onClick={() => handleClick(5)} />
-      </div>
-      <div className="board-rows">
-        <Square value={state[6]} onClick={() => handleClick(6)} />
-        <Square value={state[7]} onClick={() => handleClick(7)} />
-        <Square value={state[8]} onClick={() => handleClick(8)} />
-      </div>
+      {isWinner ? (
+        <div style={{ textAlign: "center", marginTop: "15em" }}>
+          <h1>Player {isWinner} won the game!</h1>
+          <button
+            style={{
+              border: "1px solid #fff",
+              width: "100px",
+              height: "40px",
+              borderRadius: "10px",
+              fontWeight: "700",
+            }}
+          >
+            Play Again!
+          </button>
+        </div>
+      ) : (
+        <>
+          <div className="board-rows">
+            <Square value={state[0]} onClick={() => handleClick(0)} />
+            <Square value={state[1]} onClick={() => handleClick(1)} />
+            <Square value={state[2]} onClick={() => handleClick(2)} />
+          </div>
+          <div className="board-rows">
+            <Square value={state[3]} onClick={() => handleClick(3)} />
+            <Square value={state[4]} onClick={() => handleClick(4)} />
+            <Square value={state[5]} onClick={() => handleClick(5)} />
+          </div>
+          <div className="board-rows">
+            <Square value={state[6]} onClick={() => handleClick(6)} />
+            <Square value={state[7]} onClick={() => handleClick(7)} />
+            <Square value={state[8]} onClick={() => handleClick(8)} />
+          </div>
+        </>
+      )}
     </div>
   );
 };
---
+++ vite-project/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <h1>Tic-Tac-Toe Game</h1>
+      <p>Click on any of the squares to start the game</p>
+      <p>The first player is X by default</p>
+    </div>
+  )
+}
+
+export default Header
